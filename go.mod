c02d526f3462fd104cbf0e0cc3f675453b806186
Paul Antonov <paul.antonov@gmail.com>
2022-01-03 00:04:13 +0300

    added signatures (initial revision)

--- go.mod
+++ go.mod
@@ -2,26 +2,28 @@ module github.com/pantonov/webauthn-example
 
 go 1.17
 
+replace github.com/pantonov/webauthn_sign => ../webauthn_sign
+
 require (
-	github.com/duo-labs/webauthn v0.0.0-20190926021235-9562c88a0899
+	github.com/duo-labs/webauthn v0.0.0-20211221191814-a22482edaa3b
 	github.com/duo-labs/webauthn.io v0.0.0-20190926134215-35f44a73518f
 	github.com/gorilla/mux v1.7.1
+	github.com/pantonov/webauthn_sign v0.0.0-00010101000000-000000000000
 )
 
 require (
-	github.com/agl/ed25519 v0.0.0-20170116200512-5312a6153412 // indirect
 	github.com/cloudflare/cfssl v0.0.0-20190726000631-633726f6bcb7 // indirect
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/fxamacker/cbor/v2 v2.2.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.1.0 // indirect
 	github.com/google/certificate-transparency-go v1.0.21 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.1.3 // indirect
-	github.com/katzenpost/core v0.0.7 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/sirupsen/logrus v1.4.1 // indirect
-	github.com/ugorji/go/codec v1.1.7 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
 	golang.org/x/sys v0.0.0-20190726091711-fc99dfbffb4e // indirect
 )
--- server.go
+++ server.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-    "bytes"
-    "crypto/sha256"
-    "encoding/base64"
-    "encoding/json"
-    "errors"
-    "fmt"
-    "github.com/duo-labs/webauthn/protocol/webauthncose"
-    "log"
-    "net/http"
-    "strings"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
 
-    "github.com/duo-labs/webauthn.io/session"
-    "github.com/duo-labs/webauthn/protocol"
-    "github.com/duo-labs/webauthn/webauthn"
-    "github.com/gorilla/mux"
+	"github.com/duo-labs/webauthn.io/session"
+	"github.com/duo-labs/webauthn/protocol"
+	"github.com/duo-labs/webauthn/webauthn"
+	"github.com/gorilla/mux"
+
+	"github.com/pantonov/webauthn_sign"
 )
 
 var webAuthn *webauthn.WebAuthn
@@ -26,9 +24,9 @@ func main() {
 
 	var err error
 	webAuthn, err = webauthn.New(&webauthn.Config{
-		RPDisplayName: "Foobar Corp.",     // Display Name for your site
-		RPID:          "localhost",        // Generally the domain name for your site
-		RPOrigin:      "http://localhost", // The origin URL for WebAuthn requests
+		RPDisplayName: "Foobar Corp.",          // Display Name for your site
+		RPID:          "localhost",             // Generally the domain name for your site
+		RPOrigin:      "http://localhost:8080", // The origin URL for WebAuthn requests
 		// RPIcon: "https://duo.com/logo.png", // Optional icon URL for your site
 	})
 
@@ -50,8 +48,8 @@ func main() {
 	r.HandleFunc("/login/begin/{username}", BeginLogin).Methods("GET")
 	r.HandleFunc("/login/finish/{username}", FinishLogin).Methods("POST")
 
-    r.HandleFunc("/beginsign/{username}/{sigdata}", BeginSign).Methods("GET")
-    r.HandleFunc("/sign/{username}/{sigdata}",      Sign).Methods("POST")
+	r.HandleFunc("/beginsign/{username}/{sigdata}", BeginSign).Methods("GET")
+	r.HandleFunc("/sign/{username}/{sigdata}", Sign).Methods("POST")
 
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./")))
 
@@ -176,7 +174,6 @@ func BeginLogin(w http.ResponseWriter, r *http.Request) {
 	jsonResponse(w, options, http.StatusOK)
 }
 
-
 func FinishLogin(w http.ResponseWriter, r *http.Request) {
 
 	// get username
@@ -223,196 +220,81 @@ func jsonResponse(w http.ResponseWriter, d interface{}, c int) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(c)
-	_,_ = fmt.Fprintf(w, "%s", dj)
+	_, _ = fmt.Fprintf(w, "%s", dj)
 }
 
 func BeginSign(w http.ResponseWriter, r *http.Request) {
 
-    // get username
-    vars := mux.Vars(r)
-    username := vars["username"]
-    sighash := sha256.Sum256([]byte(vars["sigdata"]))
+	// get username
+	vars := mux.Vars(r)
+	username := vars["username"]
+	sighash := sha256.Sum256([]byte(vars["sigdata"]))
 
-    // get user
-    user, err := userDB.GetUser(username)
+	// get user
+	user, err := userDB.GetUser(username)
 
-    // user doesn't exist
-    if err != nil {
-        log.Println(err)
-        jsonResponse(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	// user doesn't exist
+	if err != nil {
+		log.Println(err.Error())
+		jsonResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	options, _ := webauthn_sign.PrepareSignatureAssertion(webAuthn, sighash[:], user)
+	if err != nil {
+		log.Println("PrepareSignatureAssertion: ", err.Error())
+		jsonResponse(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    // generate PublicKeyCredentialRequestOptions, session data
-    options, sessionData, err := BeginSignReq(webAuthn, sighash[:], user)
-    if err != nil {
-        log.Println("beginSignReq: " + err.Error())
-        jsonResponse(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    // store session data as marshaled JSON
-    err = sessionStore.SaveWebauthnSession("signature", sessionData, r, w)
-    if err != nil {
-        log.Println("saveSigession: ", err.Error())
-        jsonResponse(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    jsonResponse(w, options, http.StatusOK)
-}
-
-
-func BeginSignReq(wa *webauthn.WebAuthn, sighash []byte, user *User) (*protocol.CredentialAssertion, *webauthn.SessionData, error) {
-
-    if len(sighash) < 16 || len(sighash) > 64 {
-        return nil, nil, protocol.ErrBadRequest.WithDetails("Invalid signature hash length")
-    }
-    credentials := user.WebAuthnCredentials()
-
-    if len(credentials) == 0 { // If the user does not have any credentials, we cannot do login
-        return nil, nil, protocol.ErrBadRequest.WithDetails("Found no credentials for user")
-    }
-
-    var allowedCredentials = make([]protocol.CredentialDescriptor, len(credentials))
-
-    for i, credential := range credentials {
-        var credentialDescriptor protocol.CredentialDescriptor
-        credentialDescriptor.CredentialID = credential.ID
-        credentialDescriptor.Type = protocol.PublicKeyCredentialType
-        allowedCredentials[i] = credentialDescriptor
-    }
-
-    requestOptions := protocol.PublicKeyCredentialRequestOptions{
-        Challenge:          sighash,
-        Timeout:            wa.Config.Timeout,
-        RelyingPartyID:     wa.Config.RPID,
-        UserVerification:   wa.Config.AuthenticatorSelection.UserVerification,
-        AllowedCredentials: allowedCredentials,
-    }
-    newSessionData := webauthn.SessionData{
-        Challenge:            base64.RawURLEncoding.EncodeToString(sighash),
-        UserID:               user.WebAuthnID(),
-        AllowedCredentialIDs: requestOptions.GetAllowedCredentialIDs(),
-        UserVerification:     requestOptions.UserVerification,
-    }
-
-    response := protocol.CredentialAssertion{Response: requestOptions}
-
-    return &response, &newSessionData, nil
+	jsonResponse(w, options, http.StatusOK)
 }
 
 type Signature struct {
-    AuthenticatorData protocol.URLEncodedBase64 `json:"ad"`
-    ClientData        protocol.URLEncodedBase64 `json:"cd"`
-    SignatureData     protocol.URLEncodedBase64 `json:"s"`
+	AuthenticatorData protocol.URLEncodedBase64 `json:"ad"`
+	ClientData        protocol.URLEncodedBase64 `json:"cd"`
+	SignatureData     protocol.URLEncodedBase64 `json:"s"`
 }
 
 func Sign(w http.ResponseWriter, r *http.Request) {
 
-    // get username
-    vars := mux.Vars(r)
-    username := vars["username"]
+	// get username
+	vars := mux.Vars(r)
+	username := vars["username"]
 
-    // get user
-    user, err := userDB.GetUser(username)
+	// get user
+	user, err := userDB.GetUser(username)
 
-    // user doesn't exist
-    if err != nil {
-        log.Println("getuser error:", err)
-        jsonResponse(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	// user doesn't exist
+	if err != nil {
+		log.Println(err.Error())
+		jsonResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	credential := user.credentials[0]
 
-    // load the session data
-    _, err = sessionStore.GetWebauthnSession("signature", r)
-    if err != nil {
-        log.Println("getsession error: ", err.Error())
-        jsonResponse(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	signature, err := webauthn_sign.ParseSignatureCredentialResponse(r)
+	if err != nil {
+		fmt.Println(err.Error())
+		jsonResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	valid, err := signature.VerifySha256(credential.PublicKey, []byte(vars["sigdata"]))
+	if nil != err {
+		log.Println("checksig error", err)
+		jsonResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if !valid {
+		log.Println("signature verify failed")
+		jsonResponse(w, "signature verification failed", http.StatusBadRequest)
+		return
+	} else {
+		log.Println("Signature verify OK!")
+	}
 
-    credential := user.credentials[0]
-
-    // in an actual implementation, we should perform additional checks on
-    // the returned 'credential', i.e. check 'credential.Authenticator.CloneWarning'
-    // and then increment the credentials counter
-
-    //bodyData, _ := ioutil.ReadAll(r.Body)
-    //r.Body = ioutil.NopCloser(bytes.NewReader(bodyData))
-
-
-
-    //credential, err := webAuthn.FinishLogin(user, sessionData, r)
-    //if err != nil {
-    //    log.Println("FinishLogin: ", err.Error())
-    //    jsonResponse(w, err.Error(), http.StatusBadRequest)
-    //    return
-   // }
-    //r.Body = ioutil.NopCloser(bytes.NewReader(bodyData))
-
-    parsedResponse, err := protocol.ParseCredentialRequestResponse(r)
-    if err != nil {
-        fmt.Println("ParseResponse: " + err.Error())
-        jsonResponse(w, err.Error(), http.StatusBadRequest)
-        return
-    }
-    //log.Println(parsedResponse)
-    signature := Signature {
-        AuthenticatorData: parsedResponse.Raw.AssertionResponse.AuthenticatorData,
-        ClientData:        parsedResponse.Raw.AssertionResponse.ClientDataJSON,
-        SignatureData:     parsedResponse.Response.Signature,
-    }
-    sighash := sha256.Sum256([]byte(vars["sigdata"]))
-
-    valid, err := checkSignature(webAuthn.Config, &signature, credential.PublicKey, sighash[:])
-
-    if nil != err {
-        log.Println("checksig error", err)
-        jsonResponse(w, err.Error(), http.StatusBadRequest)
-        return
-    }
-    if !valid {
-        log.Println("signature verify failed")
-        jsonResponse(w, "signature verification failed", http.StatusBadRequest)
-        return
-    } else {
-        log.Println("Signature verify OK!")
-    }
-
-    msigjs, _ := json.Marshal(signature)
-    msig := string(msigjs)
-    log.Println(msig)
-    // handle successful login
-    jsonResponse(w, msig, http.StatusOK)
-}
-
-func checkSignature(config *webauthn.Config, signature *Signature, pubkey []byte,  sigdata []byte) (bool, error) {
-
-    // to: unmarshal authenticatordata, check rpid hash
-    // rpIDHash := sha256.Sum256([]byte(config.RPID))
-    collectedClientData := protocol.CollectedClientData{}
-    if err := json.Unmarshal(signature.ClientData, &collectedClientData); nil != err {
-        return false, errors.New("unmarshalling ccd: " + err.Error())
-    }
-    var challenge protocol.URLEncodedBase64
-    if err := challenge.UnmarshalJSON([]byte(collectedClientData.Challenge)); nil != err {
-        return false, errors.New("unmarshalling challenge: " + err.Error())
-    }
-    if bytes.Compare(challenge, sigdata) != 0 {
-        log.Println("challenge mismatch")
-        return false, nil
-    }
-    clientDataHash := sha256.Sum256(signature.ClientData)
-    sigData := append(signature.AuthenticatorData, clientDataHash[:]...)
-
-    key, err := webauthncose.ParsePublicKey(pubkey)
-    if nil != err {
-        return false, errors.New("pubkey parse error: " + err.Error())
-    }
-    valid, err := webauthncose.VerifySignature(key, sigData, signature.SignatureData)
-    if nil != err {
-        return false, errors.New("verify signature error: " + err.Error())
-    }
-    return valid, nil
+	msigjs, _ := json.Marshal(signature)
+	msig := string(msigjs)
+	log.Println(msig)
+	// handle successful login
+	jsonResponse(w, msig, http.StatusOK)
 }
